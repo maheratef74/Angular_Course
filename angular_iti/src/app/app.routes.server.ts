05c2dc8190b36173e900590b79b4f47938f5ae32
Maher Atef <maheratef600@gmail.com>
2026-06-12 17:44:11 +0300

    Lap 3

--- angular_iti/src/app/app.routes.server.ts
+++ angular_iti/src/app/app.routes.server.ts
@@ -2,7 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'products/edit/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
 ];
+
--- angular_iti/src/app/app.ts
+++ angular_iti/src/app/app.ts
@@ -1,14 +1,31 @@
 import { Component, signal } from '@angular/core';
-import { Products } from '../components/products/products';
+import { RouterOutlet, Router, NavigationEnd, RouterLink, RouterLinkActive } from '@angular/router';
 import { ThemeToggleDirective } from '../directives/theme-toggle';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  imports: [Products, ThemeToggleDirective],
+  standalone: true,
+  imports: [RouterOutlet, ThemeToggleDirective, RouterLink, RouterLinkActive],
+
+
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('angular_iti');
+  showNavbar = signal(true);
+
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      let currentRoute = this.router.routerState.snapshot.root;
+      while (currentRoute.firstChild) {
+        currentRoute = currentRoute.firstChild;
+      }
+      this.showNavbar.set(currentRoute.data['hideNavbar'] !== true);
+    });
+  }
 }
 
